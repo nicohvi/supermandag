6896b5a7c42a00d43ca6af744c21d026b92a7b70
Nicolay Hvidsten <nico.hvi@gmail.com>
2013-12-11 11:00:41 +0100

    postgres debugging

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,7 +2,7 @@ class Movie < ActiveRecord::Base
   has_many :votes
 
   scope :by_votes,
-        select('*, count(votes.id) AS votes').
+        select('movies.id, title, image_url, count(votes.id) AS votes').
             joins(:votes).
             group('movies.id').
             order('votes DESC')
