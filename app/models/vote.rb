8a029684a78df2ac3b5e00ede4b5662196c12839
Nicolay Hvidsten <nico.hvi@gmail.com>
2013-12-12 10:15:19 +0100

    fu pølstein

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,4 +2,15 @@ class Vote < ActiveRecord::Base
   belongs_to :movie, counter_cache: :number_of_votes
 
   validates_uniqueness_of :username, scope: :movie_id
+
+  before_save :username_check
+
+  private
+
+  def username_check
+    Vote.where(movie_id: movie.id).each do |vote|
+      return false if ( vote.username.include?(username) || username.include?(vote.username) )
+    end
+  end
+
 end
